b0fec298fe4ac2ade81d8f33e115ca284b22b8cf
shaked-lokits <shakedl@salt.security>
2025-12-11 22:10:59 +0200

    Removed redundant lines from messy Apple Notes html.

--- src/lib/html_to_markdown.sh
+++ src/lib/html_to_markdown.sh
@@ -1,5 +1,5 @@
 html_to_markdown() {
-	cat | pandoc -f html -t gfm --wrap=none || {
+	cat | pandoc -f html -t gfm-raw_html --wrap=none | sed 's/^&nbsp;$//' | sed 's/[[:space:]]*$//' | cat -s || {
 		echo "Error: Failed to convert HTML to markdown" >&2
 		return 1
 	}
