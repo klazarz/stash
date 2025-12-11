2a3b261460f589c2bd3e0c17761490d6c2f7b317
shaked-lokits <shakedl@salt.security>
2025-12-11 21:46:51 +0200

    Fixed pull bug with leading newline.

--- src/lib/update_frontmatter.sh
+++ src/lib/update_frontmatter.sh
@@ -5,6 +5,7 @@ update_frontmatter() {
 	echo "---"
 	printf "$(echo "$content" | pcregrep -M -o1 '^---\s*([\s\S]*?)\s*^---\n?' | grep -v 'apple_notes_id')\n" | grep .
 	echo "apple_notes_id: $apple_notes_id"
-	echo "---\n"
+	echo "---"
+	echo ""
 	echo "$(echo "$content")" | pcregrep -M -v '^---\s*([\s\S]*?)\s*^---\n?'
 }
